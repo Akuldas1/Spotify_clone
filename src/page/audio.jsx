7d3dae15624498f1876f8416ff28d4ffbb80c124
Akuldas1 <akuldas651@gmail.com>
2024-02-02 22:59:24 +0530

    Commit

---
+++ src/page/audio.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+// // index.js or another component
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Playbutton from './playbutton';
+// import m1 from "../assets/audios/m1.mp3"
+// import m2 from "../assets/audios/m2.mp3"
+// import m3 from "../assets/audios/m3.mp3"
+
+// const audioSources = [
+//   m1,
+//   m2,
+//   m3
+// ];
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Playbutton audioSources={audioSources} />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
